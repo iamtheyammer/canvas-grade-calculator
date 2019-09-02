03b2f28d72843ef701306d9c182c649c09423c21
iamtheyammer <iamtheyammer@gmail.com>
2019-09-02 00:10:50 -0700

    Clean up warning from home rewrite

--- frontend/src/components/Home/index.js
+++ frontend/src/components/Home/index.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Redirect, Link } from 'react-router-dom';
+import { Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import './index.css';
 import banner from '../../assets/banner.svg'
