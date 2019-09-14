a7865376fc09d79c8f8d8c4cb0c2d58df4fc1ae9
iamtheyammer <iamtheyammer@gmail.com>
2019-09-14 16:11:45 -0700

    Fix formatting

--- frontend/src/util/getUrlPrefix.js
+++ frontend/src/util/getUrlPrefix.js
@@ -1,4 +1,5 @@
-export default process.env.NODE_ENV === 'development' ?
-  'http://localhost:8000' :
-  process.env.DEFAULT_API_URI ? 
-    process.env.DEFAULT_API_URI : '';
+export default process.env.NODE_ENV === 'development'
+  ? 'http://localhost:8000'
+  : process.env.DEFAULT_API_URI
+  ? process.env.DEFAULT_API_URI
+  : '';
