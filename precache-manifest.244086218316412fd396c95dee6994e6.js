8de975a37488c678e88564495436587f19d6b7ce
I Yam Who I Yam - Travis Deploy (from Travis CI) <iamtheyammer@gmail.com>
2020-01-28 03:48:28 +0000

    Deploy iamtheyammer/canvas-grade-calculator to github.com/iamtheyammer/canvas-grade-calculator.git:gh-pages

---
+++ precache-manifest.244086218316412fd396c95dee6994e6.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f14dee1f973007aae0fd037b1f466b52",
+    "url": "/index.html"
+  },
+  {
+    "revision": "08094c685e773822b270",
+    "url": "/static/css/2.c6d3f69e.chunk.css"
+  },
+  {
+    "revision": "821cd31e55f27dfeff2e",
+    "url": "/static/css/main.29800c5f.chunk.css"
+  },
+  {
+    "revision": "08094c685e773822b270",
+    "url": "/static/js/2.c565ffd9.chunk.js"
+  },
+  {
+    "revision": "821cd31e55f27dfeff2e",
+    "url": "/static/js/main.d9e1ad89.chunk.js"
+  },
+  {
+    "revision": "e07bff4a5068f34b430d",
+    "url": "/static/js/runtime~main.885d0f27.js"
+  },
+  {
+    "revision": "72ba0815d08c416b4fcc618f2f95f3de",
+    "url": "/static/media/banner-light.72ba0815.svg"
+  },
+  {
+    "revision": "ac4b901b37ae7ab5aa05edfe5932eb7f",
+    "url": "/static/media/banner.ac4b901b.svg"
+  },
+  {
+    "revision": "aa8e9ba4af608d92eb1c8ab4e29cfc48",
+    "url": "/static/media/pop_out.aa8e9ba4.svg"
+  }
+]);
